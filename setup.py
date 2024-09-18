a2885d9b6b78a9ab353e2ac9ebb06b5d5e753fbc
its_pores <42297782+Ameyapores@users.noreply.github.com>
2024-09-18 17:31:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,12 @@ from setuptools import find_packages, setup
 print(
     "Installing DEAR. Dependencies should already be installed with the provided conda env."
 )
+
+setup(
+    name="dear",
+    version="0.1.0",
+    packages=find_packages(),
+    description="DEAR: Disentangled Environment and Agent Representations for Reinforcement Learning without Reconstruction",
+    author="Ameya Pore, Riccardo Muradore, Diego Dall'Alba",
+    author_email="ameya.pore@univr.it",
+)
